42d1377c5531642068645ece4d84e6db1073edc2
Otavio Santana <otaviopolianasantana@gmail.com>
2022-09-20 15:55:24 +0100

    update/create jakarta-ee project
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jakarta-ee/src/main/java/org/a4j/presentation/ultrafast/jakarta/ee/JakartaeeRestApplication.java
@@ -0,0 +1,13 @@
+package org.a4j.presentation.ultrafast.jakarta.ee;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+@ApplicationScoped
+public class JakartaeeRestApplication extends Application {
+}
