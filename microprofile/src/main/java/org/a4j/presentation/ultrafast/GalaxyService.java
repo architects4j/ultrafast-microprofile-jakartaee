0eacf2f758e54341d34fbcc8e70306ebb8002df8
Otavio Santana <otaviopolianasantana@gmail.com>
2022-09-22 21:49:04 +0100

    move sample code to microprofile
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ microprofile/src/main/java/org/a4j/presentation/ultrafast/GalaxyService.java
@@ -0,0 +1,32 @@
+package org.a4j.presentation.ultrafast;
+
+import one.microstream.integrations.cdi.types.Store;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.Optional;
+
+@ApplicationScoped
+public class GalaxyService {
+
+    @Inject
+    private Galaxy galaxy;
+
+    @Store
+    public void add(CelestialBody body) {
+        this.galaxy.add(body);
+    }
+
+    @Store
+    public void delete(String name) {
+        this.galaxy.deleteById(name);
+    }
+
+    public Optional<CelestialBody> findById(String name){
+        return this.galaxy.findByName(name);
+    }
+
+    public String getNames() {
+        return this.galaxy.getCelestialNames();
+    }
+}
