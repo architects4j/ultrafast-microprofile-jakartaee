5c315e7be8adfed6aa875f6582f4639b56f383d7
Otavio Santana <otaviopolianasantana@gmail.com>
2022-09-27 08:19:38 +0100

    Update test
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- cdi-se/src/test/java/org/a4j/presentation/ultrafast/GalaxyServiceTest.java
+++ cdi-se/src/test/java/org/a4j/presentation/ultrafast/GalaxyServiceTest.java
@@ -1,14 +1,8 @@
 package org.a4j.presentation.ultrafast;
 
-import org.jboss.weld.junit5.EnableWeld;
-
-import javax.inject.Inject;
-
-
-@EnableWeld
 class GalaxyServiceTest {
 
-    @Inject
-    private GalaxyService service;
+
+
 
 }
